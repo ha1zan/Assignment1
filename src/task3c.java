93dfdb190f473e12e52509e481b84cb656374c5e
adinurjeks <korolorol26@gmail.com>
2026-09-24 10:18:05 +0600

    task3c.java

---
+++ src/task3c.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class task3c {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+         int c = b/a;
+        System.out.println(c);
+
+    }
+}
