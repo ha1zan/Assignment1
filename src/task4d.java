f7f61d334870252eaa7af4b957611bf45108c8e1
adinurjeks <korolorol26@gmail.com>
2026-09-24 10:34:09 +0600

    Merge remote-tracking branch 'origin/main'

---
+++ src/task4d.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class task4d {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+         int a = scanner.nextInt();
+         int b = scanner.nextInt();
+         int c = b%a;
+        System.out.println(c);
+    }
+}
